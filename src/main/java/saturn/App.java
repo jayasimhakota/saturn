97b29d7a65c54a6d2cd3ebdb4fc8dcaa3af7dacd
jayasimha <jayasimha.aix@gmail.com>
2018-05-24 10:08:25 +0530

    test

---
+++ src/main/java/saturn/App.java
@@ -0,0 +1,7 @@
+package saturn;
+
+public class App {
+public String sample() {
+	return("This is a sampleDemo");
+}
+}
---
+++ src/test/java/saturn/AppTest.java
@@ -0,0 +1,13 @@
+package saturn;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test()  {
+	}
+
+}
